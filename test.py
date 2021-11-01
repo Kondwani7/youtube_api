02a2acebc1c412fe49d6a508d2786f67521c7eea
Kondwani Ngulube <kondwanigideon.ngulube@gmail.com>
2021-11-01 11:58:10 +0200

    commit

--- test.py
+++ test.py
@@ -18,8 +18,40 @@ request1 = youtube.search().list(
 
 response1 = request1.execute()
 #testing out the comments resource
-response2 = youtube.comments().list(
-    id="l0U7SxXHkPY",
-    part="snippet"
-).execute
+response2 = youtube.channels().list(
+    id="UCbROEACPZuCVaAM_IzaAe1A",
+    part="contentDetails"
+).execute()
+print(response2)
 #get youtuber upload times
+
+def get_channel(channel_name):
+    return youtube.search().list(
+        q=channel_name,type="channel", part="snippet",
+    ).execute()
+
+def get_videos(channel_id, part="snippet", limit=10):
+    result = youtube.channels().list(
+        id=channel_id,part="contentDetails"
+    ).execute()
+    playlist_id= result['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+
+    videos = []
+    next_page_token = None
+
+    while 1:
+        res = youtube.playlistItems().list(
+            playlistId = playlist_id,
+            part=part,
+            maxResults=min(limit, 50),
+            pageToken=next_page_token
+        ).execute()
+
+        videos += res['items']
+        next_page_token = res.get('PageToken')
+        if next_page_token is None or len(videos) >= limit:
+            break
+        return videos
+
+
+
